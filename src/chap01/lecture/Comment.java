fafc4d42032f19a78ce9914d4aa4ebadf19a05c7
sebaek <sebaek@gmail.com>
2020-06-02 11:15:48 +0900

    comment

---
+++ src/chap01/lecture/Comment.java
@@ -0,0 +1,31 @@
+package chap01.lecture;
+
+public class Comment {
+	// 한줄 주석
+	// 또 다른 한줄 주석
+	// 주석: 프로그램 실행에 영향을 미치지 않는 텍스트
+	// 단축키: ctrl + /
+	
+	/*
+	  여러줄 주석
+	  여러줄 주석
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+//		System.out.println("Hello JAVA");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
