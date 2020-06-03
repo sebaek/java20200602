8481803343e591aebbdd0d1d42335f43651f126a
sebaek <sebaek@gmail.com>
2020-06-03 10:14:15 +0900

    not op

---
+++ src/chap03/lecture/unary/NotOperator.java
@@ -0,0 +1,14 @@
+package chap03.lecture.unary;
+
+public class NotOperator {
+	public static void main(String[] args) {
+		// !
+		// 피연산자 boolean 타입
+		// 결과도 boolean 타입
+		
+		boolean a = true;
+		boolean b = !a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
