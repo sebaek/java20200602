0277af35c0bd2460a2a7d985cc79a8844076062b
sebaek <sebaek@gmail.com>
2020-06-04 09:25:24 +0900

    chap02 exercise

---
+++ src/chap02/exercises/Exercise.java
@@ -0,0 +1,156 @@
+package chap02.exercises;
+
+public class Exercise {
+	public static void main(String[] args) {
+		// 1번 문제 : 답 4
+		int i;
+//		System.out.println(i);
+		
+		// 2번 문제 : 답 1, 4, 5
+		int modelName;
+//		int class;
+//		int 6hour;
+		int $value;
+		int _age;
+//		int int;
+		
+		// 3번 문제: 
+		// byte(1), (short, char)(2), int(4), long(8)
+		// float(4), double(8) 
+		// boolean(1)
+		
+		// 4번 문제:
+		int age;
+		age = 10;
+		double price = 3.14;
+		// 타입: int, double
+		// 변수명: age, price
+		// 리터럴: 10, 3.14
+	
+		// 5번 문제: 답 3번
+		byte byteValue = 0;
+		char charValue = 'A';
+		
+		int intValue = byteValue;
+		intValue = charValue;
+//		short shortValue = charValue;
+		double doubleValue = byteValue;
+		
+		
+		// 6번 문제:
+		intValue = 10;
+		charValue = 'A';
+		doubleValue = 5.7;
+		String strValue = "A";
+
+		double var1 = (double) intValue;
+		byte var2 = (byte) intValue;
+		int var3 = (int) doubleValue;
+//		char var4 = (char) strValue;
+
+		// 7번 문제: 3번
+		int var71 = 10;
+		long var72 = 10000000000000L;
+//		char var73 = '';
+		double var74 = 10;
+		float var75 = 10;
+
+		// 8번 문제 : 답 1번
+		byteValue = 10;
+		float floatValue = 2.5F;
+		doubleValue = 2.5;
+		
+//		byte result1 = byteValue + byteValue;
+		int result2 = 5 + byteValue;
+		float result3 = 5 + floatValue;
+		double result4 = 5 + doubleValue;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
