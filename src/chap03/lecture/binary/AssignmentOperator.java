abf2975889c585d0664e46e1fad3c07d48339f76
sebaek <sebaek@gmail.com>
2020-06-04 11:06:18 +0900

    assignment

---
+++ src/chap03/lecture/binary/AssignmentOperator.java
@@ -0,0 +1,29 @@
+package chap03.lecture.binary;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		// = += -= *= /= %=
+		// &= |= ^=
+		// <<= >>= >>>=
+		
+		int i = 3;
+		i += 5;
+		System.out.println(i);
+		i *= 2;
+		System.out.println(i);
+		i <<= 1;
+		System.out.println(i);
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
