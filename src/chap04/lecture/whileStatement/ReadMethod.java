b56a484c619781d82914d3699096fd8505d22e3a
sebaek <sebaek@gmail.com>
2020-06-05 11:28:02 +0900

    read method

---
+++ src/chap04/lecture/whileStatement/ReadMethod.java
@@ -0,0 +1,19 @@
+package chap04.lecture.whileStatement;
+
+public class ReadMethod {
+	public static void main(String[] args) throws Exception {
+		System.out.println("입력>");
+		
+		int i = System.in.read();
+		System.out.println(i);
+		
+		i = System.in.read();
+		System.out.println(i);
+		
+		i = System.in.read();
+		System.out.println(i);
+		
+		
+		
+	}
+}
---
+++ src/chap04/lecture/whileStatement/WhileStatement.java
@@ -0,0 +1,13 @@
+package chap04.lecture.whileStatement;
+
+public class WhileStatement {
+	public static void main(String[] args) {
+		//while (조건) {명령문들;;;;}
+		
+		int i = 0;
+		while (i < 3) {
+			System.out.println("실행문..");
+			i++;
+		}
+	}
+}
