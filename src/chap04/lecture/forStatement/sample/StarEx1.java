ec49b8a9271b366145de14d4fca9d4f1d376047e
sebaek <sebaek@gmail.com>
2020-06-05 11:09:20 +0900

    star

--- src/chap04/lecture/forStatement/sample/StarEx1.java
+++ src/chap04/lecture/forStatement/sample/StarEx1.java
@@ -2,6 +2,11 @@ package chap04.lecture.forStatement.sample;
 
 public class StarEx1 {
 	public static void main(String[] args) {
+//		*
+//		**
+//		***
+//		****
+//		*****
 		
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j <= i; j++) {
@@ -9,5 +14,126 @@ public class StarEx1 {
 			}
 			System.out.println();
 		}
+		
+//		*****
+//		****
+//		***
+//		**
+//		*
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < (5-i); j++) {
+				System.out.print("*"); // 반복
+			}
+			System.out.println();
+		}
+		
+//		    *
+//		   **
+//		  ***
+//		 ****
+//		*****
+		for (int i = 0; i < 5; i++) {
+			
+			// " "
+			for (int j = 0; j < (4-i); j++) {
+				System.out.print(" ");
+			}
+			// "*"
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+
+//		*****
+//		 ****
+//		  ***
+//		   **
+//		    *
+		
+		for (int i = 0; i < 5; i++) {
+			// " "
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			// "*"
+			for (int k = 0; k < (5 - i); k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
