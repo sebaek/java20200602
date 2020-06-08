e437f5c56af274931281ff52e58f14930fe12dca
sebaek <sebaek@gmail.com>
2020-06-08 15:05:57 +0900

    exercise 06, 08

--- src/chap05/exercises/Exercise08.java
+++ src/chap05/exercises/Exercise08.java
@@ -8,15 +8,33 @@ public class Exercise08 {
 				{78, 83, 93, 87, 88}
 		};
 		
+		System.out.println(array.length);
+		System.out.println(array[2].length);
+		
 		int sum = 0;
 		double avg = 0.0;
 		
 		// 작성위치 
+		int numItems = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+//				System.out.println(array[i][j]);
+				sum += array[i][j];
+				numItems++;
+			}
+		}
+		avg = (double) sum / numItems;
+				
 		
 		System.out.println("sum: " + sum);
 		System.out.println("avg: " + avg);
 	}
 }
+
+
+
+
+
 
 
 
