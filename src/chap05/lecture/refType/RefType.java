85c00d34479394f5e851582f79df8f1556ae9ab2
sebaek <sebaek@gmail.com>
2020-06-08 09:33:15 +0900

    reference type

---
+++ src/chap05/lecture/refType/RefType.java
@@ -0,0 +1,35 @@
+package chap05.lecture.refType;
+
+public class RefType {
+	public static void main(String[] args) {
+		// primitive type (기본 타입, 원시 타입)
+		// byte, short, int, long, float, double, boolean, char
+		int a = 3;
+		
+		// reference type (참조 타입)
+		// 나머지 모두 다
+		String s1 = "java";
+		String s2 = "python";
+		
+		String s3 = "java";
+		
+		// 참조값중 null 값
+		s3 = null;
+		
+		s1.length();
+		s3.length();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
