aedcc05fc590bb1381a8e56a7428053840f4baaa
sebaek <sebaek@gmail.com>
2020-06-08 11:49:15 +0900

    length

---
+++ src/codingbat/array1/Solution.java
@@ -0,0 +1,29 @@
+package codingbat.array1;
+
+public class Solution {
+	public boolean firstLast6(int[] nums) {
+
+		return nums[0] == 6 || nums[nums.length - 1] == 6;
+	}
+
+	public boolean sameFirstLast(int[] nums) {
+		if (nums.length > 0) {
+			return nums[0] == nums[nums.length - 1];
+		}
+
+		return false;
+	}
+
+	public int[] makePi() {
+		return new int[] { 3, 1, 4 };
+	}
+
+	public boolean commonEnd(int[] a, int[] b) {
+		return (a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1]);
+	}
+
+	public int sum3(int[] nums) {
+		return nums[0] + nums[1] + nums[2];
+	}
+
+}
