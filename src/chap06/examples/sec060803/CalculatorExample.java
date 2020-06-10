a1ec225fb328c28eac3a3aac4ee31432cdb94f48
sebaek <sebaek@gmail.com>
2020-06-10 11:28:54 +0900

    method invoke

---
+++ src/chap06/examples/sec060803/CalculatorExample.java
@@ -0,0 +1,8 @@
+package chap06.examples.sec060803;
+
+public class CalculatorExample {
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.execute();
+	}
+}
