f2771ec5d0b419d552ddd4fa5d8b05fa8cc3d7df
sebaek <sebaek@gmail.com>
2020-06-10 09:39:16 +0900

    method

---
+++ src/chap06/lecture/method/MethodEx1.java
@@ -0,0 +1,29 @@
+package chap06.lecture.method;
+
+public class MethodEx1 {
+	public static void main(String[] args) {
+		MyClass c1 = new MyClass();
+		
+		c1.myMethod();
+		c1.myMethod(99);
+		c1.myMethod(200);
+		
+		c1.myMethod(300, 500);
+		
+		c1.yourMethod();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/chap06/lecture/method/MyClass.java
@@ -0,0 +1,56 @@
+package chap06.lecture.method;
+
+public class MyClass {
+	// 필드
+	
+	// 생성자
+	
+	// 메소드
+	// 리턴타입 메소드명(파라미터 리스트) {
+	//     명령문들;;;;; 
+	// }
+	
+	// 메소드명 명명규칙 : lowerCamelCase
+	// 파라미터 리스트 : 타입 파라미터명, 타입 파라미터명, 타입 파라미터명,  
+	
+	void myMethod() {
+		System.out.println("마이 메소드 실행");
+	}
+	
+	void myMethod(int a) {
+		System.out.println("마이 메소드 실행");
+		System.out.println("파라미터 : " + a);
+	}
+	
+	void myMethod(int a, int b) {
+		System.out.println("마이 메소드 실행");
+		System.out.println("파라미터1번 : " + a);
+		System.out.println("파라미터2번 : " + b);
+	}
+	
+	void yourMethod() {
+		System.out.println("유어 메소드 실행");
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
