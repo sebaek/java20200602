1c315cc00a448fb1110d1e03c153e7664f9936e5
sebaek <sebaek@gmail.com>
2020-06-11 09:43:05 +0900

    package import

---
+++ src/chap06/examples/sec061204/mycompany/Car.java
@@ -0,0 +1,42 @@
+package chap06.examples.sec061204.mycompany;
+
+//import java.util.Scanner;
+import static java.lang.Math.random;
+
+import java.util.List;
+
+import chap06.examples.sec061204.hankook.SnowTire;
+import chap06.examples.sec061204.hankook.Tire;
+//import chap06.examples.sec061204.kumho.Tire;
+import chap06.examples.sec061204.hyndai.Engine;
+import chap06.examples.sec061204.kumho.BigWidthTire;
+
+public class Car {
+//	chap06.examples.sec061204.hyndai.Engine engine 
+//		= new chap06.examples.sec061204.hyndai.Engine();
+	Engine engine = new Engine();
+	SnowTire snowTire;
+	BigWidthTire bwt = new BigWidthTire();
+	Tire tire1 = new Tire();
+	chap06.examples.sec061204.kumho.Tire tire2 
+		= new chap06.examples.sec061204.kumho.Tire();
+//	Scanner scanner;
+	List list;
+	String str;
+	double d = random();
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/chap06/lecture/packageKeyword/PackageKeyword.java
@@ -0,0 +1,36 @@
+package chap06.lecture.packageKeyword;
+
+public class PackageKeyword {
+	public static void main(String[] args) {
+		// package 키워드 
+		// : 파일 맨 위에 해당 클래스의 패키지 위치를 작성
+		
+		// 명명 규칙 : lowerCamelCase or lower_snake_case
+		// $ _ 대소문자 숫자
+
+		
+		// 예시)
+		// 내가 MyClass를 mypackage에 넣고
+		// 다른 사람이 MyClass mypackage에 넣으면 안됨.
+		
+		// 상위 패키지를 회사의 도메인 명(거꾸로)으로 사용
+		
+		// com.samsung.mypackage 
+		// com.lg.mypackage
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
