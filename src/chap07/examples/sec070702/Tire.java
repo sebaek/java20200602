088c376dfbd5d2962df5ed62efb8b3f541b45d6d
sebaek <sebaek@gmail.com>
2020-06-12 12:31:38 +0900

    polymorphism

--- src/chap07/examples/sec070702/Tire.java
+++ src/chap07/examples/sec070702/Tire.java
@@ -1,5 +1,26 @@
 package chap07.examples.sec070702;
 
 public class Tire {
-
+	// 필드
+	public int maxRotation;
+	public int accumulatedRotation;
+	public String location;
+	
+	// 생성자
+	public Tire(String location, int maxRotation) {
+		this.location = location;
+		this.maxRotation = maxRotation;
+	}
+	
+	public boolean roll() {
+		++accumulatedRotation;
+		
+		if (accumulatedRotation < maxRotation) {
+			System.out.println(location + " Tire 수명: " + (maxRotation - accumulatedRotation) + "회");
+			return true;
+		} else {
+			System.out.println("*** " + location + " Tire 펑크 ***");
+			return false;
+		}
+	}
 }
