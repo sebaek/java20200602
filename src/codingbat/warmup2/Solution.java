6bbd19bccd30e40b246b57324b157527708d04ef
sebaek <sebaek@gmail.com>
2020-06-17 15:02:17 +0900

    warmup2

---
+++ src/codingbat/warmup2/Solution.java
@@ -0,0 +1,14 @@
+package codingbat.warmup2;
+
+public class Solution {
+	public String stringTimes(String str, int n) {
+		String result = "";
+
+		for (int i = 0; i < n; i++) {
+			result += str;
+		}
+
+		return result;
+	}
+
+}
