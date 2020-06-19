37d59056da29a3bac27262d61428c9cb3a72f14c
sebaek <sebaek@gmail.com>
2020-06-19 15:47:58 +0900

    warmup2

--- src/codingbat/warmup2/Solution.java
+++ src/codingbat/warmup2/Solution.java
@@ -91,4 +91,70 @@ public class Solution {
 		return count;
 	}
 
+	public int arrayCount9(int[] nums) {
+		int count = 0;
+
+		for (int n : nums) {
+			if (n == 9) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public boolean arrayFront9(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 9) {
+				return true;
+			}
+
+			if (i == 3) {
+				break;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean array123(int[] nums) {
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (nums[i] == 1) {
+				if (nums[i + 1] == 2 && nums[i + 2] == 3) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public int stringMatch(String a, String b) {
+		int loop = Math.min(a.length(), b.length());
+		loop--;
+
+		int count = 0;
+		for (int i = 0; i < loop; i++) {
+			if (a.charAt(i) == b.charAt(i)) {
+				if (a.charAt(i + 1) == b.charAt(i + 1)) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	public String stringX(String str) {
+		String result = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			if (i == 0 || i == str.length() - 1 || str.charAt(i) != 'x') {
+				result += str.charAt(i);
+			}
+		}
+
+		return result;
+	}
+
 }
